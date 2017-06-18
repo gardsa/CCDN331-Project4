1c275ebe9aeca41055a9da97c2311cb6e2fd8a73
Samantha Gard <sam.gard@abletech.co.nz>
2017-06-19 00:09:32 +1200

    werk

--- scripts.js
+++ scripts.js
@@ -25,8 +25,36 @@
     }
     else if (screenClass.contains('fifth-point')) {
       screenClass.remove('fifth-point');
-      screenClass.add('references');
+      screenClass.add('quiz-1');
+    }
+    else if (screenClass.contains('quiz-1')) {
+      screenClass.remove('quiz-1');
+      screenClass.add('quiz-2');
+    }
+    else if (screenClass.contains('quiz-2')) {
+      checkboxScore();
+      screenClass.remove('quiz-2');
+      screenClass.add('results');
     }
   });
 
+  function checkboxScore() {
+    var checkboxes = d.querySelectorAll("input[type='checkbox']"),
+        score = 0;
+
+    for (const checkbox of checkboxes) {
+      if (checkbox.checked == true){
+        score += 1;
+      }
+    }
+    printScoreResults(score);
+  }
+
+  function printScoreResults(score) {
+    if (score < 7){
+      d.getElementById('results').innerHTML += 'That you design FOR people. While this has its merits, designing for people and not with them can often to lead to unhappy or dissatisfied clients or users. Try...';
+    } else {
+      d.getElementById('results').innerHTML += 'That you design WITH people. BLAH BLAH BLAH.';
+    }
+  }
 })(document);
